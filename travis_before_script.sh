834564090e57d1526965f0bfa56117bf8d94821a
NickolasHu <huc1011@gmail.com>
2017-03-14 15:24:13 +0800

    update travis script

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,8 +1,10 @@
 set -e 
 
+ls apm/node_modules
+
 if [ ! -e apm/node_modules/atom-package-manager ]; then
   echo 'initializing apm'
-  mkdir apm/node_modules 
+  # mkdir apm/node_modules 
   cd apm/node_modules
   git clone https://github.com/TheraPackages/apm.git atom-package-manager
   cd atom-package-manager && npm install
