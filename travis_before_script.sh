7eb4894099c8a8cfe8b15877ef29a2c51219ee92
NickolasHu <huc1011@gmail.com>
2017-03-30 19:12:56 +0800

    update tool-bar symbols-tree-view

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,7 +1,5 @@
 set -e 
 
-ls apm/node_modules
-
 if [ ! -e apm/node_modules/atom-package-manager ]; then
   echo 'initializing apm'
   # mkdir apm/node_modules 
@@ -12,3 +10,5 @@ if [ ! -e apm/node_modules/atom-package-manager ]; then
 else 
   echo 'skip apm install'
 fi
+
+# echo $CERTIFICATE_OSX_P12 | base64 --decode > certificate.p12
