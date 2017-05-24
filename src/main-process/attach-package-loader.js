b4a2b4776992108b6c7ad975f61b890898c46b62
NickolasHu <huc1011@gmail.com>
2017-05-24 18:15:01 +0800

    fix debug bugs. update github.

--- src/main-process/attach-package-loader.js
+++ src/main-process/attach-package-loader.js
@@ -4,36 +4,46 @@ const path = require('path')
 const child_process = require('child_process')
 const {dialog, app} = require('electron')
 const fs = require('fs')
+const atomPaths = require('../atom-paths')
 
 // link all attach packages with apm
 
 module.exports = function () {
-  
+  atomPaths.setAtomHome(app.getPath('home'))
   const apmPath = getApmPath();
   const attachPackage = path.join(process.resourcesPath, 'attach-package');
-  console.log(`attach package path ${attachPackage}`);
-  if (fs.existsSync(attachPackage)) {
-    fs.readdir(
-      attachPackage,
-      (error, files) => {
-        if (!error) {
-          files.filter((name) => name!=="." && name!=='..').forEach((name) => {
-            if (name !== 'dumplings') {
-              child_process.execFile(
-                apmPath,
-                ['link'],
-                {cwd: path.join(attachPackage, name), stdio: 'inherit'}
-              );
-            }
-          });
-        } else {
-          console.error(error);
-        }
-    });
-  } else {
-    console.warn(`escape attach package install, cause ${attachPackage} not exists.`)
-  }
+  const _this = this;
+  fs.exists(attachPackage, (exists) => {
+    if (exists) {
+      fs.readdir(
+        attachPackage,
+        (error, files) => {
+          if (!error) {
+            const linktoPath = path.join(process.env.ATOM_HOME, 'packages');
+            files.filter((name) => name!=="." && name!=='..' && name !== 'dumplings').forEach((name) => {
+                // fs.exists(path.join(linktoPath, name), (hasThatPackage) => {
+                //   // skip packages already installed
+                //   if (!hasThatPackage) {
+                    _this.linkPackage(apmPath, path.join(attachPackage, name));
+                //   }
+                // })
+            });
+          } else {
+            console.error(error);
+          }
+      });
+    } else {
+      console.warn(`escape attach package install, cause ${attachPackage} not exists.`);
+    }
+  })
+}
 
+function linkPackage(apmPath, packagePath) {
+  child_process.execFile(
+    apmPath,
+    ['link'],
+    {cwd: packagePath, stdio: 'inherit'}
+  );
 }
 
 function getApmPath() {
