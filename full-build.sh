b3435a116a07d885d1df991082d0801023e4a01a
dengxu <hzdengxu@163.com>
2017-06-22 17:26:36 +0800

    :arrow_up: thera version && Fix symbols-tree-view issue

--- full-build.sh
+++ full-build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+npm install 
 
-script/build --install --code-sign --compress-artifacts --deploy
+if [ $? == 0 ]; then
+	npm rebuild --runtime=electron --target=1.3.15 --disturl=https://atom.io/download/atom-shell --abi=49
+fi
+
+if [ $? == 0 ]; then
+	script/build --install --code-sign --compress-artifacts --deploy
+fi
